1698a462418f08c67ecce87ca8cc25ed401b4b2d
sokas <aernestolh@gmail.com>
2023-02-23 10:45:31 +0100

    Creacion de funcionalidad para ver los diferentes estados de los dispositivos

--- lib/model/device.dart
+++ lib/model/device.dart
@@ -1,13 +1,18 @@
+
 import 'package:flutter/foundation.dart';
 
 class Device{
   String id, nombre, tipo;
-  Device({required this.id, required this.nombre,required this.tipo});
+  Uint8List imgAbierto, imgCerrado, imgEspera;
+  Device({required this.id, required this.nombre,required this.tipo, required this.imgAbierto, required this.imgCerrado, required this.imgEspera});
   factory Device.fromJson(Map<String, dynamic> json){
     return Device(
       id: json['id'], 
       nombre: json['nombre'], 
-      tipo: json['tipo']
+      tipo: json['tipo'],
+      imgAbierto: Uint8List.fromList(json['imgAbierto']['data'].cast<int>()),
+      imgCerrado: Uint8List.fromList(json['imgCerrado']['data'].cast<int>()),
+      imgEspera: Uint8List.fromList(json['imgEspera']['data'].cast<int>()),
       );
   }
 }
--- lib/ui/pages/carga.dart
+++ lib/ui/pages/carga.dart
@@ -13,6 +13,7 @@ class Carga extends StatefulWidget {
 
 class _CargaState extends State<Carga> {
   late Future<Device?> futureDevice;
+  String estado="Espera";
 
   @override
   void initState() {
@@ -24,55 +25,52 @@ class _CargaState extends State<Carga> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: FutureBuilder<Device?>(
-            future: futureDevice,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text("${snapshot.data!.nombre} ${snapshot.data!.id} ${snapshot.data!.tipo}");
-                
-              } else if (snapshot.hasError) {
+     return Column(
+       children: [
+         Card(
+           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+           margin: EdgeInsets.all(15),
+            elevation: 10,
+           child: Column(
+             mainAxisSize: MainAxisSize.min,
+             children: <Widget>[
+            ListTile(
+          contentPadding: EdgeInsets.fromLTRB(15, 10, 25, 0),
+          title: Text('Dispositivo', textAlign: TextAlign.center,),
+           leading: Icon(Icons.devices_sharp),
+                   ),
+               FutureBuilder<Device?>(
+                 future:futureDevice,
+                 builder: ((context, snapshot){
+                   if(snapshot.hasData){
+                    switch (estado) {
+                      case "Espera":
+                        return Image.memory(snapshot.data!.imgEspera, width: 300, height: 300,);
+                        case "Abierto":
+                        return Image.memory(snapshot.data!.imgAbierto, width: 300, height: 300,);
+                        case "Cerrado":
+                        return Image.memory(snapshot.data!.imgCerrado, width: 300, height: 300,);
+                      default:
+                      return Image.memory(snapshot.data!.imgEspera, width: 300, height: 300,);
+                    }
+                     //return Text("Nombre Dispositivo: ${snapshot.data!.nombre} Tipo: ${snapshot.data!.tipo}");
+                    
+                   } else if (snapshot.hasError) {
                 return Text('${snapshot.error}');
               }
-        
-        
-        /*Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Flexible(
-              flex: 6,
-              child: Container(
-                child: Text(
-                  "Esperando",
-                  style: TextStyle(fontSize: 42),
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 14,
-              child: Image(
-                image: NetworkImage("https://i.imgur.com/kBxCOFq.png"),
-              ),
-            ),
-            Flexible(
-                flex: 10,
-                child: SizedBox(
-                  height: 100,
-                  width: 150,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Conectarse',
-                        style: TextStyle(fontSize: 22)),
-                  ),
-                ))
-          ],
-        ),*/
-        return const CircularProgressIndicator();
-            }
-            
-      ),
-      )
-    );
+                return const CircularProgressIndicator();
+                 }
+               )
+               ),
+               Row(
+                 children: <Widget>[const SizedBox(height: 20),]
+               ),
+             ],
+           ),
+         ),
+       ],
+       
+     );
+     
   }
 }
